27e106c4a86f1274e63269ef24ee7c50f3485553
Tim Weller <timweller@gmx.de>
2020-01-08 22:28:03 +0100

    initial commit for yd60mq

---
+++ keyboards/yd60mq/keymaps/timbooo/keymap.c
@@ -0,0 +1,42 @@
+#include QMK_KEYBOARD_H
+
+/* enable underglow on startup, configure color and disable again */
+void keyboard_post_init_user(void) {
+    rgblight_enable_noeeprom();
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+    rgblight_sethsv_noeeprom_white();
+    wait_ms(2000);
+    rgblight_disable_noeeprom();
+}
+
+/* enable underglow when caps lock is active */
+bool led_update_user(led_t led_state) {
+    static uint8_t caps_state = 0;
+    if (caps_state != led_state.caps_lock) {
+        led_state.caps_lock ? rgblight_enable_noeeprom() : rgblight_disable_noeeprom();
+        caps_state = led_state.caps_lock;
+    }
+    return true;
+}
+
+
+/* keymaps */
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC,
+		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,          KC_ENT,
+		KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, MO(1),
+		KC_LCTL, KC_LALT, KC_LGUI,                   MO(1),   KC_SPC,  MO(1),                     KC_RALT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+	),
+	[1] = LAYOUT(
+		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		_______, _______, _______,                   _______, RGB_TOG, _______,                   _______, KC_MPRV, KC_MSTP, KC_MPLY, KC_MNXT
+	),
+
+};
+// clang-format on
